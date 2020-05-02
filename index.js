197e9738f33dd5b6e239dfa5f19fb647e14cb370
Bharat Gupta <bharat.gupta@flipkart.com>
2020-05-02 17:23:31 +0530

    added fibonacci series

--- index.js
+++ index.js
@@ -2,17 +2,47 @@
 (function() {
 	var btnSquare = document.getElementById('btnSquare');
 	var txtInput = document.getElementById('txtInput');
+	var txtFibonacciInput = document.getElementById('txtFibonacciInput');
+	var btnFibonacci = document.getElementById('btnFibonacci');
+	var lblMsg = document.getElementById('lblMsg');
+	var lblfbMsg = document.getElementById('lblfbMsg');
 
-	btnSquare.addEventListener('click', getAlert);
+	btnSquare.addEventListener('click', getSqaure);
+	btnFibonacci.addEventListener('click',getFibonacciSeries);
 
-	function getAlert() {
+	function getSqaure() {
 		var num = txtInput.value;
 
 		if(isNaN(num)) {
-			document.getElementById('lblMsg').innerText="Invalid Input!!!";
+			lblMsg.innerText="Invalid Input!!!";
 		}
 		else {
-			document.getElementById('lblMsg').innerText=  num * num;
+			lblMsg.innerText = num * num;
+		}
+	}
+
+	// [0, 1, 1, 2, 3, 5] 
+	function getFibonacciSeries() {
+		var noOfTerms = txtFibonacciInput.value;
+
+		if(isNaN(noOfTerms) || noOfTerms == 0) {
+			lblfbMsg.innerText="Invalid Input!!!";
+		}
+		else {
+			var result = [];
+			
+			for(var i = 0; i < noOfTerms; i++) {
+				if (i <= 1) {
+					result.push(i);
+				}
+				else {
+					var nextTerm = result[i-1] + result[i-2];
+					result.push(nextTerm);
+				}
+
+				lblfbMsg.innerText = result;
+			}
+			
 		}
 	}
 })(); // iife - immedialtely invoked fucntion expression
